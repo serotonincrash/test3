12e268b4d6e0fd3143c1ebbdb02bc1a98a00df78
BuildTools <unconfigured@null.spigotmc.org>
2019-11-20 01:22:25 +0800

    got rid of unnecesscary functions

--- app/server.py
+++ app/server.py
@@ -57,11 +57,7 @@ async def homepage(request):
 
 @app.route('/analyze', methods=['POST'])
 async def analyze(request):
-    img_data = await request.form()
-    img_bytes = await (img_data['file'].read())
-    img = open_image(BytesIO(img_bytes))
-    prediction = learn.predict(img)[0]
-    return JSONResponse({'result': str(prediction)})
+    return JSONResponse({'result': "pretty cool!"})
 
 
 if __name__ == '__main__':
--- app/static/client.js
+++ app/static/client.js
@@ -1,22 +1,8 @@
 var el = x => document.getElementById(x);
 
-function showPicker() {
-  el("file-input").click();
-}
-
-function showPicked(input) {
-  el("upload-label").innerHTML = input.files[0].name;
-  var reader = new FileReader();
-  reader.onload = function(e) {
-    el("image-picked").src = e.target.result;
-    el("image-picked").className = "";
-  };
-  reader.readAsDataURL(input.files[0]);
-}
 
 function analyze() {
-  var uploadFiles = el("file-input").files;
-  if (uploadFiles.length !== 1) alert("Please select a file to analyze!");
+  alert("button pressed!")
 
   el("analyze-button").innerHTML = "Analyzing...";
   var xhr = new XMLHttpRequest();
