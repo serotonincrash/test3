e2892dd8f8079ce9d736630fdc2111af3b859c7a
Sean Wong <derpydm@gmail.com>
2019-11-20 11:40:14 +0800

    try to parse text from xhr in py

--- app/server.py
+++ app/server.py
@@ -65,5 +65,5 @@ async def analyze(request):
 
 if __name__ == '__main__':
     if 'serve' in sys.argv:
-        uvicorn.run(app=app, host='0.0.0.0', port=5000, log_level="info")
+        uvicorn.run(app=app, host='0.0.0.0', port=5000, log_level="debug")
 
