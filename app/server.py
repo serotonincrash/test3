09c11ca68a8c840256bf416ff73f9633224dd5e1
Sean Wong <derpydm@gmail.com>
2019-11-20 18:27:31 +0800

    use new model download link and reverted setup to original

--- app/server.py
+++ app/server.py
@@ -28,9 +28,9 @@ async def download_file(url, dest):
                 f.write(data)
 
 async def setup_learner():
-    await download_file(export_file_url, path / export_file_name, cpu = True)
+    await download_file(export_file_url, path / export_file_name)
     try:
-        learn = load_learner(path, export_file_name)
+        learn = load_learner(path, export_file_name, cpu = True)
         return learn
     except RuntimeError as e:
         if len(e.args) > 0 and 'CPU-only machine' in e.args[0]:
