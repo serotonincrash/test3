9c21967440fed46aea472ed539ed329649b2ca4e
BuildTools <unconfigured@null.spigotmc.org>
2019-11-20 21:55:45 +0800

    i think it thinks its a textclassifierlearner

--- app/server.py
+++ app/server.py
@@ -9,9 +9,9 @@ from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
 export_file_name = 'export.pkl'
-
+del globals()["RNNLearner.TextClassifierLearner"]
 path = Path(__file__).parent
-del globals()['TextClassifierLearner']
+
 
 app = Starlette()
 app.add_middleware(CORSMiddleware, allow_origins=['*'], allow_headers=['X-Requested-With', 'Content-Type'])
