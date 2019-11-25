0384cb597d8e28300e4570c4274750b6b828d5ed
BuildTools <unconfigured@null.spigotmc.org>
2019-11-25 23:16:51 +0800

    changed temperature, changed HTML, annotated code. this will be a template.

--- app/server.py
+++ app/server.py
@@ -8,15 +8,26 @@ from starlette.applications import Starlette
 from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
+
+'''
+datafile name and url are here
+place your contruct/model name and urls here also, they are downloaded in setup_learner()
+make sure that the link gives the file itself (if you click on the link it should start the download or show the raw file)
+'''
+
 data_file_url = 'https://www.dropbox.com/s/p2bqt5yrmievgts/data_lm.pkl?raw=1'
 data_file_name = 'data_lm.pkl'
 path = Path(__file__).parent
 
 
+#start Starlette webserver, change PyTorch device to cpu to ensure no errors with Render
 app = Starlette()
 app.add_middleware(CORSMiddleware, allow_origins=['*'], allow_headers=['X-Requested-With', 'Content-Type'])
 app.mount('/static', StaticFiles(directory='app/static'))
 defaults.device = torch.device('cpu')
+
+
+# Function for downloading files
 async def download_file(url, dest):
     if dest.exists(): return
     async with aiohttp.ClientSession() as session:
@@ -25,8 +36,13 @@ async def download_file(url, dest):
             with open(dest, 'wb') as f:
                 f.write(data)
 
+# setup the AI construct
 async def setup_learner():
-    await download_file(data_file_url, path / data_file_name)
+    """
+    load the model here
+    i had issues loading the construct I had which is why I simply used WT103, please use a pretrained model
+    """
+    await download_file(data_file_url, path / data_file_name) # copy/oaste this for any file downloads you might need (construct, model etc)
     data_lm = load_data(path, file='data_lm.pkl')
     try:
         learn = language_model_learner(data_lm, AWD_LSTM, pretrained=URLs.WT103, drop_mult=0.5)
@@ -39,7 +55,7 @@ async def setup_learner():
         else:
             raise
 
-
+# function to start initialising the learner, server does not run until this is complete
 loop = asyncio.get_event_loop()
 tasks = [asyncio.ensure_future(setup_learner())]
 learn = loop.run_until_complete(asyncio.gather(*tasks))[0]
@@ -54,11 +70,14 @@ async def homepage(request):
 
 @app.route('/analyze', methods=['POST'])
 async def analyze(request):
+    # get the text from POST request
     req = await request.form()
     TEXT = req['entered-text']
-    N_WORDS = 45
+    # you may want to add input for N_WORDS and N_SENTENCES - for the latter remember to use list comprehension
+    N_WORDS = 300
     N_SENTENCES = 1
-    prediction = "\n" + learn.predict(TEXT, n_words = N_WORDS, temperature = 1.0)
+    # mess around with the temperature a bit - you may get better results
+    prediction = "\n" + learn.predict(TEXT, n_words = N_WORDS, temperature = 0.75)
     return JSONResponse({'result': str(prediction)})
 
 
